8d04fff82d79306a643a392aa432690f1d019eac
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-05 17:05:27 +0900

    add gold coins

--- RPG/src/entity/Human.java
+++ RPG/src/entity/Human.java
@@ -1,9 +1,21 @@
 package entity;
 
 public class Human extends Unit {
+	
+	private int gp;
 
 	public Human(String name, int MAX_HP, int hp, int damage) {
 		super(name, MAX_HP, hp, damage);
 	}
+
+	public int getGp() {
+		return gp;
+	}
+
+	public void setGp(int gp) {
+		this.gp = gp;
+	}
+	
+	
 
 }
