f692fb0c7335ca26a115893d6407351a2d3350cf
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-05 15:27:07 +0900

    Unit entity create

--- RPG/src/entity/Monster.java
+++ RPG/src/entity/Monster.java
@@ -1,5 +1,11 @@
 package entity;
 
-public class Monster {
+public class Monster extends Unit {
+	
+	private int drop;
+
+	public Monster(String name, int MAX_HP, int hp, int damage) {
+		super(name, MAX_HP, hp, damage);
+	}
 
 }
