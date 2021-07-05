c6157341eaa6b690dfaac6496ca688d5c19b1dbc
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-05 15:26:49 +0900

    Unit entity create

---
+++ RPG/src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import entity.Human;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ RPG/src/entity/Human.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Human extends Unit {
+
+	public Human(String name, int MAX_HP, int hp, int damage) {
+		super(name, MAX_HP, hp, damage);
+	}
+
+}
