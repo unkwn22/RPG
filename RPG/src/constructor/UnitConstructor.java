00de700f9786e5e136113b2e7e39df3bc7760def
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-05 17:36:36 +0900

    added basic player settings in constructor

--- RPG/src/constructor/UnitConstructor.java
+++ RPG/src/constructor/UnitConstructor.java
@@ -1,8 +1,10 @@
 package constructor;
 
+import entity.Human;
+
 public class UnitConstructor {
 	
-	public UnitConstructor() {
-		
+	public UnitConstructor(String name, int gender) {
+		Human player = new Human(name, 10, 10, 3, 100, gender);
 	}
 }
