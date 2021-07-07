b1a0772f81eb4e1718e1dac8fbede30a905ab31a
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-07 20:55:29 +0900

    passing item data through constructor

--- RPG/src/app/Main.java
+++ RPG/src/app/Main.java
@@ -1,22 +1,30 @@
 package app;
 
+import java.util.ArrayList;
+
+import constructor.ItemConstructor;
 import entity.Human;
+import entity.Potion;
+import entity.Weapon;
 import handler.Start;
 import handler.Town;
 
 public class Main {
-	
 	Start s = new Start();
 	Human player = null;
+	ItemConstructor i = new ItemConstructor();
+	ArrayList<Weapon> weapon = null;
+	ArrayList<Potion> potion = null;
 	
 	public Main() {
-		player = s.CharacterSettings();
-		Town t = new Town(player);
+		player = s.characterSettings();
+		weapon = i.initWeapons();
+		potion = i.initPotions();
+		Town t = new Town(player, weapon, potion);
 		t.town();
 	}
 
 	public static void main(String[] args) {
 		new Main();
 	}
-
 }
--- RPG/src/handler/Town.java
+++ RPG/src/handler/Town.java
@@ -1,14 +1,20 @@
 package handler;
 
+import java.util.ArrayList;
+
 import entity.Human;
+import entity.Potion;
+import entity.Weapon;
 
 public class Town {
 	
 	static Human player = null;
+	Shop s = null;
 	Inventory inven = new Inventory();
 	
-	public Town(Human player) {
+	public Town(Human player, ArrayList<Weapon> weapons, ArrayList<Potion> potions) {
 		Town.player = player;
+		s = new Shop(weapons, potions);
 	}
 	
 	public void town() {
@@ -18,7 +24,9 @@ public class Town {
 			System.out.println("1. Battle | 2. Shop | 3. Inventory | 4. Save | 5. Quit");
 			int choice = Start.input.NumInput();
 			if(choice == 1) continue;
-			else if(choice == 2) continue;
+			else if(choice == 2) {
+				s.mainShop();
+			}
 			else if(choice == 3) {
 				inven.playerInventory();
 			}
