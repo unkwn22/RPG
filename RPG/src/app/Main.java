589ee8c741d7eda00e247b88fd078b944eb2b411
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-07 13:49:23 +0900

    Main class source added

--- RPG/src/app/Main.java
+++ RPG/src/app/Main.java
@@ -1,11 +1,22 @@
 package app;
 
 import entity.Human;
+import handler.Start;
+import handler.Town;
 
 public class Main {
+	
+	Start s = new Start();
+	Human player = null;
+	
+	public Main() {
+		player = s.CharacterSettings();
+		Town t = new Town(player);
+		t.town();
+	}
 
 	public static void main(String[] args) {
-		
+		new Main();
 	}
 
 }
