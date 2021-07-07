5350248a43ba11025e7ccd1189ab47b4ff2dd996
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-07 14:24:01 +0900

    added conditions

--- RPG/src/handler/Town.java
+++ RPG/src/handler/Town.java
@@ -4,10 +4,11 @@ import entity.Human;
 
 public class Town {
 	
-	Human player = null;
+	static Human player = null;
+	Inventory inven = new Inventory();
 	
 	public Town(Human player) {
-		this.player = player;
+		Town.player = player;
 	}
 	
 	public void town() {
@@ -15,7 +16,12 @@ public class Town {
 			System.out.println("Name: " + player.getName() + " | HP: " + player.getMAX_HP() + "/" 
 		+ player.getHp() + " | Coins: " + player.getGp() + " | Gender: " + player.getGender());
 			System.out.println("1. Battle | 2. Shop | 3. Inventory | 4. Save | 5. Quit");
-			Start.input.NumInput();
+			int choice = Start.input.NumInput();
+			if(choice == 1) continue;
+			else if(choice == 2) continue;
+			else if(choice == 3) {
+				inven.playerInventory();
+			}
 		}
 	}
 }
