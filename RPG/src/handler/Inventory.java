11396a4ea72ff6d524b4f169bf1bb3a39541bab2
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-08 21:59:24 +0900

    removed instance and added item initiation

--- RPG/src/handler/Inventory.java
+++ RPG/src/handler/Inventory.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 import entity.Potion;
 import entity.Weapon;
-import handler.inven.PotionInven;
-import handler.inven.WeaponInven;
 
 public class Inventory {
 	
@@ -13,19 +11,31 @@ public class Inventory {
 	static ArrayList<Potion> pList = new ArrayList<>();
 	static ArrayList<Weapon> current = new ArrayList<>();
 	
-	WeaponInven w = new WeaponInven();
-	PotionInven p = new PotionInven();
+	InvenInit init = new InvenInit();
 	
 	public void playerInventory() {
 		while(true) {
-			System.out.println("1. Weapons | 2. Potions | 0. Exit");
+			System.out.println("1. Equip | 2. Use | 3. Toss | 0. Exit");
 			int choice = Start.input.NumInput();
 			if(choice == 0) {
 				break;
 			}else if(choice == 1) {
-				
+				init.equipWeapon();
 			}else if(choice == 2) {
-				
+				init.usePotion();
+			}else if(choice == 3) {
+				while(true) {
+					System.out.println("Which item would you like to toss?");
+					System.out.println("1. Weapon | 2. Potion | 0. Back");
+					choice = Start.input.NumInput();
+					if(choice == 0) {
+						break;
+					}else if(choice == 1) {
+						init.tossWeapon();
+					}else if(choice == 2) {
+						init.tossPotion();
+					}
+				}
 			}
 		}
 	}
