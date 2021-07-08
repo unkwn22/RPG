29a86298f30b3fb8620be6930cb5cd0b2b3e2acd
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-08 17:07:47 +0900

    changed variable settings and added instance

--- RPG/src/handler/Inventory.java
+++ RPG/src/handler/Inventory.java
@@ -2,24 +2,30 @@ package handler;
 
 import java.util.ArrayList;
 
+import entity.Potion;
+import entity.Weapon;
 import handler.inven.PotionInven;
 import handler.inven.WeaponInven;
 
 public class Inventory {
 	
-	static WeaponInven w = new WeaponInven();
-	static PotionInven p = new PotionInven();
+	static ArrayList<Weapon> wList = new ArrayList<>();
+	static ArrayList<Potion> pList = new ArrayList<>();
+	static ArrayList<Weapon> current = new ArrayList<>();
+	
+	WeaponInven w = new WeaponInven();
+	PotionInven p = new PotionInven();
 	
 	public void playerInventory() {
 		while(true) {
-			System.out.println("1. Equipments | 2. Potions | 0. Exit");
+			System.out.println("1. Weapons | 2. Potions | 0. Exit");
 			int choice = Start.input.NumInput();
 			if(choice == 0) {
 				break;
 			}else if(choice == 1) {
-				w.weaponInven();
+				
 			}else if(choice == 2) {
-				p.potionInven();
+				
 			}
 		}
 	}
