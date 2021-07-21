ce07f1df1f10bc9f2daf8bc8cf80a25cfc69befc
Jason <unkwn22@naver.com>
2021-07-21 17:47:31 +0900

    feat: unit constructor for monster

--- RPG/src/handler/Town.java
+++ RPG/src/handler/Town.java
@@ -3,6 +3,7 @@ package handler;
 import java.util.ArrayList;
 
 import entity.Human;
+import entity.Monster;
 import entity.Potion;
 import entity.Weapon;
 
@@ -10,12 +11,14 @@ public class Town {
 	
 	static Human player = null;
 	Shop s = null;
+	Battle b = null;
 	Inventory inven = new Inventory();
 	
 	//constructor only called once from app.Main class
-	public Town(Human player, ArrayList<Weapon> weapons, ArrayList<Potion> potions) {
+	public Town(Human player, ArrayList<Weapon> weapons, ArrayList<Potion> potions, ArrayList<Monster> monster) {
 		Town.player = player;
 		s = new Shop(weapons, potions);
+		b = new Battle(monster);
 	}
 	
 	public void town() {
@@ -23,18 +26,18 @@ public class Town {
 			System.out.print("Name: " + player.getName() + " | HP: " + player.getMAX_HP() + "/" 
 			+ player.getHp() + " | Coins: " + player.getGp() + " | Equipped: ");
 			if(Inventory.current.size() == 0) {
-				System.out.print("nothing");
+				System.out.print("nothing" + " | Damage: " + player.getDamage());
 			}else {
-				System.out.print(Inventory.current.get(0).getName());
+				System.out.print(Inventory.current.get(0).getName() + " | Damage: " + player.getDamage());
 			}
 			System.out.print(" | Gender: " + player.getGender() + "\n");
 			System.out.println("1. Battle | 2. Shop | 3. Inventory | 4. Save | 5. Quit");
 			int choice = Start.input.NumInput();
-			if(choice == 1) continue;
-			else if(choice == 2) {
+			if(choice == 1) {
+				b.viewBattle();
+			}else if(choice == 2) {
 				s.mainShop();
-			}
-			else if(choice == 3) {
+			}else if(choice == 3) {
 				inven.playerInventory();
 			}else if(choice == 4) {
 				
